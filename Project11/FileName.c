d645828c3ae1a12178f13e2125cd3bc51bba6d3d
동완 서 <k01026297619@gmail.com>
2025-06-09 00:00:18 +0900

    2-9돌발 퀘스트 고양이랑 가위바위보 해서 이기기 및  2-32-4 코드 수정

--- Project11/FileName.c
+++ Project11/FileName.c
@@ -18,7 +18,9 @@ int main(void) {
     int cat = 3;
     int past_cat = cat;
     int cp = 0;
-    int mood = 3;
+    int mood = 0;
+    int player, computer;
+    int turn_count = 0;
     int shop_choice;
     const char* item[] = { "장난감 쥐", "레이저 포인터", "스크래처", "캣타워" };
     int item_cost[] = { 1, 2, 4, 6 };
@@ -28,7 +30,6 @@ int main(void) {
     int interaction_order[2] = { -1, -1 };
     int interaction_count = 0;
     int pos;
-
 
 
     printf("**** 야옹이와 수프 ****\n\n");
@@ -52,13 +53,65 @@ int main(void) {
     srand(time(NULL));
 
     while (1) {
+        turn_count++;
+        if (turn_count % 3 == 0) {
+            printf("=== 돌발 퀘스트! ===\n");
+            printf("%s가 가위바위보 대결을 신청합니다.\n", str);
+            printf("%s와 가위바위보 게임에서 이기십시오.\n", str);
+            while (1) {
+                printf("가위(1) 바위(2) 보(3) >> ");
+                scanf_s("%d", &player);
+
+
+                if (player < 1 || player > 3) {
+                    printf("다시 입력하세요.\n");
+                    continue;
+                }
+
+
+                computer = rand() % 3 + 1;
+
+
+                if (player == 1)
+                    printf("Player: 가위\n");
+                else if (player == 2)
+                    printf("Player: 바위\n");
+                else
+                    printf("Player: 보\n");
+
+
+                if (computer == 1)
+                    printf("Computer: 가위\n");
+                else if (computer == 2)
+                    printf("Computer: 바위\n");
+                else
+                    printf("Computer: 보\n");
+
+
+                if (player == computer) {
+                    printf("비겼습니다.\n");
+                    printf("다시 시도 하세요.\n");
+                }
+                else if ((player == 1 && computer == 3) ||
+                    (player == 2 && computer == 1) ||
+                    (player == 3 && computer == 2)) {
+                    printf("Player가 이겼습니다.\n");
+                    break;
+                }
+                else {
+                    printf("Computer가 이겼습니다.\n");
+                    printf("다시 시도 하세요.\n");
+                }
+                Sleep(1000);
+                printf("\n");
+            }
+        }
         Sleep(2500);
         system("cls");
 
         printf("=============== 현재 상태 ===============\n");
         printf("CP:%d 포인트\n", cp);
         printf("%s 기분(0~3):%d\n", str, mood);
-
         printf("현재까지 만든 수프: %d개\n", soup);
         printf("집사와의 관계 (0~4): %d\n", relationship);
 
@@ -93,7 +146,6 @@ int main(void) {
             printf("%s(은)는 기분이 그대로입니다.\n", str);
         }
 
-
         // 고양이가 이동하는 코드
         if (mood == 0) {
             if (cat > HME_POS) {
@@ -101,7 +153,28 @@ int main(void) {
                 printf("기분이 매우 나쁜 %s은(는) 집으로 향합니다.\n", str);
             }
         }
-
+        else if (mood == 1) {
+            // 가장 가까운 놀잇감 찾기
+            int closest_dist = ROOM_WIDTH;
+            int target_pos = -1;
+            for (int i = 0; i < ROOM_WIDTH; i++) {
+                if (item_pos[i] == 1 || item_pos[i] == 2) { // 스크래처(S) 또는 캣타워(T)
+                    int dist = abs(i - cat);
+                    if (dist < closest_dist) {
+                        closest_dist = dist;
+                        target_pos = i;
+                    }
+                }
+            }
+            if (target_pos != -1) {
+                if (cat < target_pos) cat++;
+                else if (cat > target_pos) cat--;
+                printf("%s(은)는 심심해서 스크래처 쪽으로 이동합니다.\n", str);
+            }
+            else {
+                printf("놀잇감이 없어서 기분이 매우 나빠집니다.\n");
+            }
+        }
         else if (mood == 2) {
             printf("%s(은)는 기분좋게 식빵을 굽고 있습니다.\n", str);
 
@@ -114,89 +187,80 @@ int main(void) {
         }
 
 
-
         printf("\n");
         Sleep(500);
 
-
-        // 이동조건에 맞는 값을 출력하는 코드
         if (cat == HME_POS) {
+
             if (past_cat == HME_POS) {
                 mood++;
-                printf("%s(은)는 자신의 집에서 편안함을 느낍니다: %d->%d\n", str, mood - 1, mood);
+                printf("집에서 쉼을 취한 %s(은)는 기분이 조금 좋아집니다: %d->%d\n", str, mood - 1, mood);
             }
         }
-
         else if (cat == BWL_POS) {
             printf("%s(은)는 수프를 만들었습니다!\n", str);
-
-            int soup_type = rand() % 3;
-            if (soup_type == 0)
-                printf("감자 수프를 만들었습니다!\n");
-            else if (soup_type == 1)
-                printf("양송이 수프를 만들었습니다!\n");
-            else if (soup_type == 2)
-                printf("브로콜리 수프를 만들었습니다!\n");
-
+            const char* soups[] = { "감자 수프", "양송이 수프", "브로콜리 수프" };
+            printf("%s를 만들었습니다!\n", soups[rand() % 3]);
             soup++;
             printf("현재까지 만든 수프 수: %d개\n\n", soup);
         }
+        else if (item_pos[cat] == 1) {
+            // 스크래처
+            mood++;
+            printf("%s(은)는 스크래처를 긁고 놀았습니다. 기분이 조금 좋아졌습니다: %d->%d\n", str, mood - 1, mood);
+        }
+        else if (item_pos[cat] == 2) {
+            // 캣타워
+            mood += 2;
+            printf("%s(은)는 캣타워를 뛰어다닙니다. 기분이 제법 좋아졌습니다: %d->%d\n", str, mood - 2, mood);
+        }
+
         Sleep(500);
 
-
-
+        // 방 출력
         for (int i = 0; i < ROOM_WIDTH; i++) printf("#");
         printf("\n");
-
         for (int i = 0; i < ROOM_WIDTH; i++) {
-            if (i == 0)
-                printf("#");
-            else if (i == HME_POS)
-                printf("H");
-            else if (i == BWL_POS)
-                printf("B");
-            else if (i == ROOM_WIDTH - 1)
-                printf("#");
-            else
-                printf(" ");
+            if (i == 0) printf("#");
+            else if (i == HME_POS) printf("H");
+            else if (i == BWL_POS) printf("B");
+            else if (i == ROOM_WIDTH - 1) printf("#");
+            else printf(" ");
         }
         printf("\n");
 
         for (int i = 0; i < ROOM_WIDTH; i++) {
-            if (i == 0)
-                printf("#");
-            else if (i == cat)
-                printf("C");
-            else if (i == past_cat && cat != past_cat)
-                printf(".");
-            else if (i == ROOM_WIDTH - 1)
-                printf("#");
-            else
-                printf(" ");
+            if (i == 0) printf("#");
+            else if (i == cat) printf("C");
+            else if (item_pos[i] == 1) printf("S");
+            else if (item_pos[i] == 2) printf("T");
+            else if (i == ROOM_WIDTH - 1) printf("#");
+            else printf(" ");
         }
         printf("\n");
 
         for (int i = 0; i < ROOM_WIDTH; i++) printf("#");
         printf("\n\n");
-        //상호작용선택하는 부분
-        printf("어떤 상호작용을 하시겠습니까? 0. 아무것도 하지 않음 1. 긁어 주기\n ");
 
-        while (1) {
-            printf(">>");
+        // 상호작용
+        printf("어떤 상호작용을 하시겠습니까?\n");
+        printf("0. 아무것도 하지 않음\n");
+        printf("1. 긁어 주기\n");
+        for (int i = 0; i < interaction_count; i++) {
+            int idx = interaction_order[i];
+            printf("%d. %s와 놀기\n", i + 2, item[idx]);
+        }
+        // 입력값이 유효할 때까지 반복
+        printf(">> ");
+        scanf_s("%d", &interaction);
+        while (interaction < 0 || interaction > interaction_count + 1) {
+            printf("잘못된 입력입니다. 다시 입력하세요.\n");
+            printf(">> ");
             scanf_s("%d", &interaction);
-
-            if (interaction == 0 || interaction == 1) {
-                break;
-            }
-            else {
-
-            }
         }
 
         Sleep(500);
-
         number = rand() % 6 + 1;
-
         if (interaction == 0) {
             printf("아무것도 하지 않습니다.\n");
             if (mood < 0) {
@@ -249,66 +313,65 @@ int main(void) {
             printf("현재 친밀도: %d\n\n", relationship);
 
         }
+       
 
-
-    }
-    cp = mood - 1 + relationship;
-    printf("CP: %d 포인트\n", cp);
-    printf("\n상점에서 물건을 살 수 있습니다.\n");
-    printf("0. 아무것도 사지 않는다\n");
-    for (int i = 0; i < 4; i++) {
-        printf("%d. %s: %d CP", i + 1, item[i], item_cost[i]);
-        if (item_purchased[i]) printf(" (품절)");
-        printf("\n");
-    }
-    // 입력값이 유효할 때까지 반복
-    printf(">> ");
-    scanf_s("%d", &shop_choice);
-    while (shop_choice < 0 || shop_choice > 4) {
-        printf("잘못된 입력입니다. 다시 입력하세요.\n");
+        cp = mood - 1 + relationship;
+        printf("CP: %d 포인트\n", cp);
+        printf("\n상점에서 물건을 살 수 있습니다.\n");
+        printf("0. 아무것도 사지 않는다\n");
+        for (int i = 0; i < 4; i++) {
+            printf("%d. %s: %d CP", i + 1, item[i], item_cost[i]);
+            if (item_purchased[i]) printf(" (품절)");
+            printf("\n");
+        }
+        // 입력값이 유효할 때까지 반복
         printf(">> ");
         scanf_s("%d", &shop_choice);
-    }
+        while (shop_choice < 0 || shop_choice > 4) {
+            printf("잘못된 입력입니다. 다시 입력하세요.\n");
+            printf(">> ");
+            scanf_s("%d", &shop_choice);
+        }
 
-    if (shop_choice == 0) {
-        printf("구매하지 않았습니다.\n\n");
-    }
-    else if (shop_choice >= 1 && shop_choice <= 4) {
-        int index = shop_choice - 1;
-        if (cp < item_cost[index]) {
-            printf("CP가 부족합니다.\n\n");
+        if (shop_choice == 0) {
+            printf("구매하지 않았습니다.\n\n");
         }
-        else if (item_purchased[index]) {
-            printf("이미 구매한 아이템입니다.\n\n");
-        }
-        else {
-            item_purchased[index] = true;
-            cp -= item_cost[index];
-            printf("%s를 구매했습니다. 남은 CP: %d 포인트\n\n", item[index], cp);
-            // 장난감(0,1번)만 구매 순서 저장
-            if (index == 0 || index == 1) {
-                if (interaction_count < 2) {
-                    interaction_order[interaction_count++] = index;
+        else if (shop_choice >= 1 && shop_choice <= 4) {
+            int index = shop_choice - 1;
+            if (cp < item_cost[index]) {
+                printf("CP가 부족합니다.\n\n");
+            }
+            else if (item_purchased[index]) {
+                printf("이미 구매한 아이템입니다.\n\n");
+            }
+            else {
+                item_purchased[index] = true;
+                cp -= item_cost[index];
+                printf("%s를 구매했습니다. 남은 CP: %d 포인트\n\n", item[index], cp);
+                // 장난감(0,1번)만 구매 순서 저장
+                if (index == 0 || index == 1) {
+                    if (interaction_count < 2) {
+                        interaction_order[interaction_count++] = index;
+                    }
+                }
+                if (index == 2 || index == 3) {
+
+                    pos = rand() % (ROOM_WIDTH - 2) + 1; // 1~13 사이
+                    while (pos == HME_POS || pos == BWL_POS || pos == cat) {
+                        pos = rand() % (ROOM_WIDTH - 2) + 1;
+                    }
+
+                    if (index == 2) {
+                        item_pos[pos] = 1; // 스크래처
+                    }
+                    else {
+                        item_pos[pos] = 2; // 캣타워
+                    }
+
+                    printf("%s를 방의 위치 %d에 배치했습니다.\n\n", item[index], pos);
                 }
             }
-            if (index == 2 || index == 3) {
-
-                pos = rand() % (ROOM_WIDTH - 2) + 1; // 1~13 사이
-                while (pos == HME_POS || pos == BWL_POS || pos == cat) {
-                    pos = rand() % (ROOM_WIDTH - 2) + 1;
-                }
-
-                if (index == 2) {
-                    item_pos[pos] = 1; // 스크래처
-                }
-                else {
-                    item_pos[pos] = 2; // 캣타워
-                }
-
-                printf("%s를 방의 위치 %d에 배치했습니다.\n\n", item[index], pos);
-            }
         }
-    }
     }
 
     return 0;
